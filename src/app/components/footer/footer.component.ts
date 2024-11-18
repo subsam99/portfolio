450cafe90dd65cfd1fb8dcaff2614776843da850
subsam99 <syahmi.aimanm11@gmail.com>
2024-11-18 18:22:20 +0800

    added footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  repoLink: string = 'https://github.com/subsam99/portfolio'
+}
