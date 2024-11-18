1f9dfa2b3882edbd40a22d39dbefcc49f3413556
subsam99 <syahmi.aimanm11@gmail.com>
2024-11-18 18:23:20 +0800

    set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { ShowcaseComponent } from './pages/showcase/showcase.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'showcase', component: ShowcaseComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: NotFoundComponent },
+];
