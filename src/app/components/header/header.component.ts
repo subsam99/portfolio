22a3890efeaa05888e577d06ab1cdcd5eca5802a
subsam99 <syahmi.aimanm11@gmail.com>
2024-11-19 17:21:57 +0800

    add mobile view for header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,40 @@
+import { NgClass, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavigationStart, Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgClass,
+    NgTemplateOutlet
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   ghLink: string = 'https://github.com/subsam99'
   inLink: string = 'https://linkedin.com/in/syahmiaimanm'
+
+  menuOpen: boolean = false
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.resetState();
+      }
+    })
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen
+  }
+
+  resetState() {
+    this.menuOpen = false;
+  }
 }
